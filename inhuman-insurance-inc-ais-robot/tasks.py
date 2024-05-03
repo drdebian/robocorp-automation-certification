30855b5994d24325a2df68ab2a1a8f07d339c3d7
Albert Ulmer <albert@ulmer.name>
2024-05-03 10:58:41 +0200

    transform JSON into table

--- inhuman-insurance-inc-ais-robot/tasks.py
+++ inhuman-insurance-inc-ais-robot/tasks.py
@@ -1,7 +1,13 @@
 from robocorp.tasks import task
 from RPA.HTTP import HTTP
+from RPA.JSON import JSON
+from RPA.Tables import Tables
 
 http = HTTP()
+json = JSON()
+table = Tables()
+
+TRAFFIC_JSON_FILE_PATH = "output/traffic.json"
 
 
 @task
@@ -12,9 +18,11 @@ def produce_traffic_data():
     """
     http.download(
         url="https://github.com/robocorp/inhuman-insurance-inc/raw/main/RS_198.json",
-        target_file="output/traffic.json",
+        target_file=TRAFFIC_JSON_FILE_PATH,
         overwrite=True,
     )
+    traffic_data = load_traffic_data_as_table()
+    table.write_table_to_csv(traffic_data, "output/test.csv")
 
 
 @task
@@ -24,3 +32,8 @@ def consume_traffic_data():
     Consumes traffic data work items.
     """
     print("consume")
+
+
+def load_traffic_data_as_table():
+    json_data = json.load_json_from_file(TRAFFIC_JSON_FILE_PATH)
+    return table.create_table(json_data["value"])
