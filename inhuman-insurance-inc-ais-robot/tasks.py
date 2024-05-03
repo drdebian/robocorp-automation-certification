b533bf54d184008ebc092c6bc16c701e49b72ea5
Albert Ulmer <albert@ulmer.name>
2024-05-03 10:52:59 +0200

    download raw traffic data

---
+++ inhuman-insurance-inc-ais-robot/tasks.py
@@ -0,0 +1,26 @@
+from robocorp.tasks import task
+from RPA.HTTP import HTTP
+
+http = HTTP()
+
+
+@task
+def produce_traffic_data():
+    """
+    Inhuman Insurance, Inc. Artificial Intelligence System automation.
+    Produces traffic data work items.
+    """
+    http.download(
+        url="https://github.com/robocorp/inhuman-insurance-inc/raw/main/RS_198.json",
+        target_file="output/traffic.json",
+        overwrite=True,
+    )
+
+
+@task
+def consume_traffic_data():
+    """
+    Inhuman Insurance, Inc. Artificial Intelligence System robot.
+    Consumes traffic data work items.
+    """
+    print("consume")
